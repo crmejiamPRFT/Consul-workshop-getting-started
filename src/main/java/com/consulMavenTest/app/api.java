adf44230836e1a3042a2e634254d3d6fe7b9d78e
crmejiamPRFT <cristian.mejiam@perficient.com>
2023-02-17 16:09:29 -0500

    Renombrar clase api a Api y crear API inicial

--- src/main/java/com/consulMavenTest/app/api.java
+++ src/main/java/com/consulMavenTest/app/api.java
@@ -1,8 +1,35 @@
 package com.consulMavenTest.app;
 
-public class api {
+import static java.lang.Float.parseFloat;
+import static spark.Spark.*;
+public class Api {
 
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        Calculadora calculadora = new Calculadora();
+        get("/hello/:name", (req, res) -> "Hello " + req.params(":name"));
+
+        get("/calculator/suma/:num1/:num2", (req, res) -> {
+            float num1 = parseFloat(req.params(":num1"));
+            float num2 = parseFloat(req.params(":num2"))
+            return calculadora.suma(num1, num2);
+        });
+
+        get("/calculator/resta/:num1/:num2", (req, res) -> {
+            float num1 = parseFloat(req.params(":num1"));
+            float num2 = parseFloat(req.params(":num2"))
+            return calculadora.resta(num1, num2);
+        });
+
+        get("/calculator/multiplicacion/:num1/:num2", (req, res) -> {
+            float num1 = parseFloat(req.params(":num1"));
+            float num2 = parseFloat(req.params(":num2"))
+            return calculadora.multiplicacion(num1, num2);
+        });
+
+        get("/calculator/division/:num1/:num2", (req, res) -> {
+            float num1 = parseFloat(req.params(":num1"));
+            float num2 = parseFloat(req.params(":num2"))
+            return calculadora.division(num1, num2);
+        });
     }
 }
